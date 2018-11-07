68d0ced89e6315199f22b6aa8cbd2d4868f0a1de
Cristian Vergara <cvergarapulgar@gmail.com>
2018-11-07 02:10:00 -0300

    Versión Beta Proyecto Web

--- automotora/app/views.py
+++ automotora/app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, HttpResponse
-
+from .forms import VehiculoForm
+from .models import Vehiculo
 # Create your views here.
 
 def home(request):
     return render(request, 'registro.html')
+
+def registro(request):
+    return render(request, 'registroauto.html')
+
+def auto_vista_test(request):
+    form = VehiculoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'registrotest.html', context)
